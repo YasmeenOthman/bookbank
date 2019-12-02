be5f0ebacc98372f3ccea81338c7740c6becc58a
noursaqqa <noursaqqa7@gmail.com>
2019-12-02 10:49:57 +0200

    initial first card for books

--- src/ItemsPage/items.tsx
+++ src/ItemsPage/items.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
-//import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const Items: React.FC = () => {
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 349,
+  },
+  media: {
+    height: 250,
+  },
+});
+
+export default function MediaCard() {
+  const classes = useStyles();
+
   return (
-    <div>
-    </div>
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://www.vasansbookstall.com/wp-content/uploads/2016/07/img722-422x600.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            The HISTORY OF ENGLISH LITERATURE
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Owner: Nour Saqqa
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
-
-export default Items;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Items from './ItemsPage/items'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Items />, document.getElementById('root'));
 
 serviceWorker.unregister();
