154598cec27b8c5786553c1baef565d78d8468cd
AlaaAlagha <lulla.alagha@gmail.com>
2019-12-02 10:38:27 +0200

    ItemPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 //import './App.css';
+// import Item from './ItemPage/item'
 import Home from '../src/HomePage/Home'
+import NavBar from './HomePage/NavBar'
 const App: React.FC = () => {
   return (
     <div>
+      <NavBar/>
       <Home />
+      {/* <Item /> */}
     </div>
   );
 }
