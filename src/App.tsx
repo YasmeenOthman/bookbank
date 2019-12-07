e8bb0e881e7571df716e37c6efb4e4a9c5ca0965
noursaqqa <noursaqqa7@gmail.com>
2019-12-07 14:48:25 +0200

    items page

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,18 @@ import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 // createStore(reducer, allInfo)
 const App: React.FC = () => {
-  var path = window.location.href;
-  var univId = parseInt(path[path.length - 1]);
+
   return (
     <Router>
       <div className= "App">
         {/* <Home /> */}
-      <ItemsPage />
+      {/* <ItemsPage /> */}
         {/* <Item />  */}
-        {/* <Switch> 
+        <Switch> 
               <Route exact path='/' component={Home} />
              <Route exact path={`/university/:id`} component={ItemsPage} /> 
               <Route exact path={'/university/:univId/book/:bookId'} component={Item} />
-           </Switch>  */}
+           </Switch> 
       </div>
     </Router>
   );
