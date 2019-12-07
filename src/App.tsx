fd4151fe0598526b7bcf747dc72077589ef33891
noursaqqa <noursaqqa7@gmail.com>
2019-12-07 10:31:16 +0200

    react router ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 // import axios from 'axios';
 //import './App.css';
-// import Item from './ItemPage/item'
+import ItemsPage from './ItemsPage/containers/js/itemsPage.jsx';
 import Home from '../src/HomePage/Home';
-// import { createStore } from 'redux';
-// function reducer(state:any, action:any){
-//   return state
-// }
+import Item from './ItemPage1/item.jsx';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+// import Home from '../src/HomePage/Home';
+
 
 // createStore(reducer, allInfo)
 const App: React.FC = () => {
+  var path = window.location.href;
+  var univId = parseInt(path[path.length - 1]);
   return (
-    
-    <div>
-      {/* <Home /> */}
-      {/* <Login /> */}
-
-      {/* <Item /> */}
-      {/* <NavBar/> */}
-    </div>
+    <Router>
+      <div className= "App">
+        {/* <Home /> */}
+      <ItemsPage />
+        {/* <Item />  */}
+        {/* <Switch> 
+              <Route exact path='/' component={Home} />
+             <Route exact path={`/university/:id`} component={ItemsPage} /> 
+              <Route exact path={'/university/:univId/book/:bookId'} component={Item} />
+           </Switch>  */}
+      </div>
+    </Router>
   );
 };
 
