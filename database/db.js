bbabaede17e7874d48426dc44bdae518700570f8
tasneemalhaj <zakarneh.tasneem@gmail.com>
2019-12-08 12:17:40 +0200

    ignore package-lock

--- database/db.js
+++ database/db.js
@@ -1,120 +1,116 @@
-var mongoose = require("mongoose");
-require("mongoose-query-random");
-mongoose.set("useCreateIndex", true);
-mongoose.set("useUnifiedTopology", true);
+var mongoose = require('mongoose');
+require('mongoose-query-random');
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
 //-------------------MongoURI----------------------------
-const URI =
-  "mongodb+srv://bookbank:bookbank@book-bank-3ough.mongodb.net/test?retryWrites=true&w=majority";
+const URI = 'mongodb+srv://bookbank:bookbank@book-bank-3ough.mongodb.net/test?retryWrites=true&w=majority';
 
 //-------------------Connection Setup-------------------------
 mongoose.connect(URI, function(err, db) {
-  if (err) {
-    console.log(
-      "Unable to connect to the server. Please start the server. Error:",
-      err
-    );
-  } else {
-    console.log("Mongoose Connected to Server successfully!");
-  }
+	if (err) {
+		console.log('Unable to connect to the server. Please start the server. Error:', err);
+	} else {
+		console.log('Mongoose Connected to Server successfully!');
+	}
 });
 
 var db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
-  // we're connected!
-  console.log("We're Connected: TAZ");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	// we're connected!
+	console.log("We're Connected: TAZ");
 });
 
 //=======================================================
 //-------------------Book Schema-------------------------
 //=======================================================
 var booksSchema = mongoose.Schema({
-  id: { type: Number, unique: true },
-  bookName: { type: String },
-  bookCover: { type: String },
-  bookDescription: { type: String },
-  universityId: { type: Number },
-  createdAt: { type: Date, default: Date.now }
+	id: { type: Number, unique: true },
+	bookName: { type: String },
+	bookCover: { type: String },
+	bookDescription: { type: String },
+	universityId: { type: Number },
+	createdAt: { type: Date, default: Date.now }
 });
 
 //-------------------Book Model-------------------------
-let Book = mongoose.model("books", booksSchema);
+let Book = mongoose.model('books', booksSchema);
 
 var saveBook = function(book) {
-  console.log("in save function");
-  var newBook = new Book({
-    id: book.id,
-    bookName: book.bookName,
-    bookCover: book.bookCover,
-    bookDescription: book.bookDescription,
-    universityId: book.universityId,
-    createdAt: book.createdAt
-  });
+	console.log('in save function');
+	var newBook = new Book({
+		id: book.id,
+		bookName: book.bookName,
+		bookCover: book.bookCover,
+		bookDescription: book.bookDescription,
+		universityId: book.universityId,
+		createdAt: book.createdAt
+	});
 
-  newBook.save(function(err, res) {
-    if (err) {
-      throw err;
-    }
-    console.log("this book is added now", res);
-  });
+	newBook.save(function(err, res) {
+		if (err) {
+			throw err;
+		}
+		console.log('this book is added now', res);
+	});
 };
 
 //=======================================================
 //-------------------Donated Book Schema-------------------------
 //=======================================================
 var donatedBooksSchema = mongoose.Schema({
-  id: { type: Number, unique: true },
-  userId: { type: Number },
-  bookId: { type: Number },
-  availability: { type: Boolean },
-  createdAt: { type: Date, default: Date.now }
+	id: { type: Number, unique: true },
+	userId: { type: Number },
+	bookId: { type: Number },
+	availability: { type: Boolean },
+	createdAt: { type: Date, default: Date.now }
 });
 
 //-------------------Donated Book Model-------------------------
-let DonatedBook = mongoose.model("donated-books", donatedBooksSchema);
+let DonatedBook = mongoose.model('donated-books', donatedBooksSchema);
 
 var saveDonatedBook = function(donatedBook) {
-  console.log("in save function");
-  var newDonatedBook = new DonatedBook({
-    id: donatedBook.id,
-    userId: donatedBook.userId,
-    bookId: donatedBook.bookId,
-    availability: donatedBook.availability,
-    createdAt: donatedBook.createdAt
-  });
+	console.log('in save function');
+	var newDonatedBook = new DonatedBook({
+		id: donatedBook.id,
+		userId: donatedBook.userId,
+		bookId: donatedBook.bookId,
+		availability: donatedBook.availability,
+		createdAt: donatedBook.createdAt
+	});
 
-  newDonatedBook.save(function(err, res) {
-    if (err) {
-      throw err;
-    }
-    console.log("this donated book has been added ", res);
-  });
+	newDonatedBook.save(function(err, res) {
+		if (err) {
+			throw err;
+		}
+		console.log('this donated book has been added ', res);
+	});
 };
 
 //=======================================================
 //-------------------User Schema-------------------------
 //=======================================================
 var userSchema = mongoose.Schema({
-  email: { type: String },
-  password: { type: String }
+	email: { type: String },
+	password: { type: String }
 });
 
 //-------------------User Model-------------------------
-var User = mongoose.model("user", userSchema);
+var User = mongoose.model('user', userSchema);
 
 var saveUser = function(user) {
-  var newUser = new User({
-    email: user.email,
-    password: user.password
-  });
+	var newUser = new User({
+		email: user.email,
+		password: user.password
+	});
 
-  newUser.save(function(err, res) {
-    if (err) {
-      throw err;
-    }
-    console.log("this user was added now", res);
-  });
+	newUser.save(function(err, res) {
+		if (err) {
+			throw err;
+		}
+		console.log('this user was added now', res);
+	});
 };
 
 //=======================================================
@@ -122,31 +118,31 @@ var saveUser = function(user) {
 //=======================================================
 
 var profileSchema = mongoose.Schema({
-  id: { type: Number, unique: true },
-  userId: { type: Number },
-  userName: { type: String },
-  universityId: { type: Number },
-  userAvatar: { type: String }
+	id: { type: Number, unique: true },
+	userId: { type: Number },
+	userName: { type: String },
+	universityId: { type: Number },
+	userAvatar: { type: String }
 });
 
 //-------------------Profile Model-------------------------
-var Profile = mongoose.model("profiles", profileSchema);
+var Profile = mongoose.model('profiles', profileSchema);
 
 var saveProfile = function(profile) {
-  var newProfile = new Profile({
-    id: profile.id,
-    userId: profile.userId,
-    userName: profile.userName,
-    universityId: profile.universityId,
-    userAvatar: profile.userAvatar
-  });
+	var newProfile = new Profile({
+		id: profile.id,
+		userId: profile.userId,
+		userName: profile.userName,
+		universityId: profile.universityId,
+		userAvatar: profile.userAvatar
+	});
 
-  newProfile.save(function(err, res) {
-    if (err) {
-      throw err;
-    }
-    console.log("this profile has been was added", res);
-  });
+	newProfile.save(function(err, res) {
+		if (err) {
+			throw err;
+		}
+		console.log('this profile has been was added', res);
+	});
 };
 
 //=======================================================
@@ -154,25 +150,25 @@ var saveProfile = function(profile) {
 //=======================================================
 
 var universitySchema = mongoose.Schema({
-  id: { type: Number, unique: true },
-  universityName: { type: String }
+	id: { type: Number, unique: true },
+	universityName: { type: String }
 });
 
 //-------------------University Model----------------------
-var University = mongoose.model("universities", universitySchema);
+var University = mongoose.model('universities', universitySchema);
 
 var saveUniversity = function(uni) {
-  var newUniversity = new University({
-    id: uni.id,
-    universityName: uni.universityName
-  });
+	var newUniversity = new University({
+		id: uni.id,
+		universityName: uni.universityName
+	});
 
-  newUniversity.save(function(err, res) {
-    if (err) {
-      throw err;
-    }
-    console.log("this University has been was added", res);
-  });
+	newUniversity.save(function(err, res) {
+		if (err) {
+			throw err;
+		}
+		console.log('this University has been was added', res);
+	});
 };
 
 //=======================================================
@@ -181,63 +177,56 @@ var saveUniversity = function(uni) {
 
 //-------------------Find 4 random universities ---------
 var findRandomUnis = function(callback) {
-  University.find().random(4, true, callback);
+	University.find().random(4, true, callback);
 };
 //--------- Find  recently added Books ---------
 var findRecentlyAddedBooks = function(callback) {
-  Book.find()
-    .sort({ createdAt: "desc" })
-    .limit(3)
-    .exec(callback);
+	Book.find().sort({ createdAt: 'desc' }).limit(4).exec(callback);
 };
 
 //--------- count Donated Books ---------
 var countDonatedBooks = function(callBack) {
-  DonatedBook.count({}, callBack);
+	DonatedBook.count({}, callBack);
 };
 
 //--------- count Universities ---------
 var countUniversities = function(callBack) {
-  University.count({}, callBack);
+	University.count({}, callBack);
 };
 
 //--------- count Users---------
 var countUsers = function(callBack) {
-  User.count({}, callBack);
+	User.count({}, callBack);
 };
 
 //--------- get the books of a university---------
 var getBooksOfUniversity = function(univId, callBack) {
-  Book.find({ universityId: univId })
-    .sort({ createdAt: "desc" })
-    .exec(callBack);
+	Book.find({ universityId: univId }).sort({ createdAt: 'desc' }).exec(callBack);
 };
 
 //---------get bluePrint book from its Id ---------
 var getbluePrintBook = function(bluePrintId, callBack) {
-  Book.findOne({ id: bluePrintId }).exec(callBack);
+	Book.findOne({ id: bluePrintId }).exec(callBack);
 };
 
 //--------- get the donated books from the bluePrint Book Id ---------
 var getDonatedBooks = function(bluePrintId, callBack) {
-  DonatedBook.find({ bookId: bluePrintId })
-    .sort({ createdAt: "asc" })
-    .exec(callBack);
+	DonatedBook.find({ bookId: bluePrintId }).sort({ createdAt: 'asc' }).exec(callBack);
 };
 
 //-------- get usres names of donated books from profile collection -------
 var getDonatedBooksOwnersName = function(usersId, callBack) {
-  Profile.find({ userId: { $in: usersId } }, callBack);
+	Profile.find({ userId: { $in: usersId } }, callBack);
 };
 
 //----- Git user's Profile ---------
 var getUserProfie = function(userId, callBack) {
-  Profile.findOne({ id: userId }).exec(callBack);
+	Profile.findOne({ id: userId }).exec(callBack);
 };
 
 // -------- get all Universities ---------
 var getAllUniversities = function(callBack) {
-  University.find({}).exec(callBack);
+	University.find({}).exec(callBack);
 };
 
 module.exports.saveBook = saveBook;
--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,28 @@ import React from 'react';
 import ItemsPage from './ItemsPage/containers/js/itemsPage.jsx';
 import Home from '../src/HomePage/Home';
 import Item from './ItemPage1/item.jsx';
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // import Home from '../src/HomePage/Home';
 
-
 // createStore(reducer, allInfo)
 const App: React.FC = () => {
-  var path = window.location.href;
-  var univId = parseInt(path[path.length - 1]);
-  return (
-
-    <Router>
-      <div className= "App">
-        {/* <Home /> */}
-      <ItemsPage />
-        {/* <Item />  */}
-        {/* <Switch> 
-              <Route exact path='/' component={Home} />
-             <Route exact path={`/university/:id`} component={ItemsPage} /> 
-              <Route exact path={'/university/:univId/book/:bookId'} component={Item} />
-           </Switch>  */}
-      </div>
-    </Router>
-  );
+	var path = window.location.href;
+	var univId = parseInt(path[path.length - 1]);
+	return (
+		<Router>
+			<div className="App">
+				{/* <Home /> */}
+				{/* <ItemsPage /> */}
+				{/* <Item />  */}
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path={`/university/:id`} component={ItemsPage} />
+					<Route exact path={'/university/:univId/book/:bookId'} component={Item} />
+				</Switch>
+			</div>
+		</Router>
+	);
 };
 
 export default App;
