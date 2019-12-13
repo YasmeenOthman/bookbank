f79e829f17cda65a5eadb36c0878a69fec6aa8e0
tasneemalhaj <zakarneh.tasneem@gmail.com>
2019-12-13 20:07:38 +0200

    fix item page & create add donated book route

--- server/routers/university.js
+++ server/routers/university.js
@@ -1,66 +1,62 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const bookBankDB = require("../../database/db.js");
+const bookBankDB = require('../../database/db.js');
 
 //---------------- Universities Page --------------------------
-router.route("/").get(function (req, res) {
-  bookBankDB.getAllUniversities(function (err, allUniversities) {
-    if (err) throw err;
-    //console.log(allUniversities);
-    res.json(allUniversities);
-  });
+router.route('/').get(function(req, res) {
+	bookBankDB.getAllUniversities(function(err, allUniversities) {
+		if (err) throw err;
+		//console.log(allUniversities);
+		res.json(allUniversities);
+	});
 });
 
 //----------------Items Page Route --------------------------
-router.route("/:univId").get(function (req, res) {
-  const univId = req.params.univId;
-  bookBankDB.getBooksOfUniversity(univId, function (err, booksOFTheUniversity) {
-    if (err) throw err;
-    //console.log(booksOFTheUniversity);
-    res.json(booksOFTheUniversity);
-  });
+router.route('/:univId').get(function(req, res) {
+	const univId = req.params.univId;
+	bookBankDB.getBooksOfUniversity(univId, function(err, booksOFTheUniversity) {
+		if (err) throw err;
+		//console.log(booksOFTheUniversity);
+		res.json(booksOFTheUniversity);
+	});
 });
 
 //----------------- Item Page Route----------------------------
-router.route("/:univId/book/:bookId").get(function (req, res) {
-  var bookId = req.params.bookId;
+router.route('/:univId/book/:bookId').get(function(req, res) {
+	console.log('serving the item page route');
+	var bookId = req.params.bookId;
 
-  var itemPageData = {};
+	var itemPageData = {
+		bluePrintBook: {},
+		donatedBooks: [],
+		donatedBooksOwners: []
+	};
 
-  //---- get bluePrint book ----
-  bookBankDB.getbluePrintBook(bookId, function (err, bluePrintBook) {
-    if (err) throw err;
-    //console.log(bluePrintBook);
-    itemPageData["bluePrintBook"] = bluePrintBook;
+	//---- get bluePrint book ----
+	bookBankDB.getbluePrintBook(bookId, function(err, bluePrintBook) {
+		if (err) throw err;
+		//console.log(bluePrintBook);
+		itemPageData['bluePrintBook'] = bluePrintBook;
 
-    //---- get donated books of that bluePrint book ----
-    bookBankDB.getDonatedBooks(bookId, function (err, donatedBooks) {
-      if (err) throw err;
+		//---- get donated books of that bluePrint book ----
+		bookBankDB.getDonatedBooks(bookId, function(err, donatedBooks) {
+			if (err) throw err;
 
-      //console.log(donatedBooks);
-      itemPageData["donatedBooks"] = donatedBooks;
+			//console.log(donatedBooks);
+			itemPageData['donatedBooks'] = donatedBooks;
 
-      //get Users Id of the donated books
-      var usersId = donatedBooks.map(function (doc) {
-        return doc.userId;
-      });
-      //console.log(usersId);
+			//get Users Id of the donated books
+			var usersId = donatedBooks.map(function(doc) {
+				return doc.userId;
+			});
 
-      // get Users Name of the donated books
-      // async function innerGetNames(usersId) {
-      //   var ownersName = await bookBankDB.getDonatedBooksOwnersName(usersId);
-      //   itemPageData["donatedBooksOwners"] = ownersName;
-      //   res.json(itemPageData);
-      // }
-      // innerGetNames(usersId);
-
-      bookBankDB.getDonatedBooksOwnersName(usersId, function (err, profiles) {
-        if (err) throw err;
-        itemPageData["donatedBooksOwners"] = profiles;
-        res.json(itemPageData);
-      });
-    });
-  });
+			bookBankDB.getDonatedBooksOwnersName(usersId, function(err, profiles) {
+				if (err) throw err;
+				itemPageData['donatedBooksOwners'] = profiles;
+				res.json(itemPageData);
+			});
+		});
+	});
 });
 
 module.exports = router;
