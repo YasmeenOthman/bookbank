2504c22256d53762ab26f58505471d904f899352
Nazeh-Taha <nazeh3036@gmail.com>
2019-12-20 22:08:19 +0200

    make it better

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use("/profile", profileRouter);
 if(process.env.NODE_ENV === "production"){
   app.use(express.static("build"));
   
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
